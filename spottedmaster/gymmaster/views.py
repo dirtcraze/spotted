575fff2bf8caa72e4fbf9036abba1439ae718b70
dirt_craze <kamil.porebski@icloud.com>
2023-01-14 02:19:21 +0100

    działa wysyłanie do bazy danych

--- spottedmaster/gymmaster/views.py
+++ spottedmaster/gymmaster/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect
+from django.urls import resolve
 from .forms import MessageForm
 from .models import *
 
@@ -8,20 +9,20 @@ def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
 
-
 def handle_form(request):
   form = MessageForm(request.POST or None)
   if request.method == "POST" and form.is_valid():
     message = form.cleaned_data["message"]
-    if request.path == "/fitnessforlife/":
+    view_name = resolve(request.path_info).url_name
+    if view_name == "fitnessforlife":
       Fitnessforlife.objects.create(message=message)
-    elif request.path == "/fitnesstrzykorony/":
+    elif view_name == "fitnesstrzykorony":
       Fitnesstrzykorony.objects.create(message=message)
-    elif request.path == "/halnygym/":
+    elif view_name == "halnygym":
       Halnygym.objects.create(message=message)
-    elif request.path == "/oxygym/":
+    elif view_name == "oxygym":
       Oxygym.objects.create(message=message)
-    elif request.path == "/xtremefitness/":
+    elif view_name == "xtremefitness":
       Xtremefitness.objects.create(message=message)
     return redirect("/")
   return render(request, "template.html", {"form": form})
