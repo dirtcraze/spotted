f2e6620f82fc688c2acd4478e1edeb7264b09ba0
dirt_craze <kamil.porebski@icloud.com>
2023-01-20 17:52:46 +0100

    podzielony front end

--- spottedmaster/gymmaster/forms.py
+++ spottedmaster/gymmaster/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class MessageForm(forms.Form):
-    message = forms.CharField(widget=forms.Textarea(attrs={'maxlength': 250, 'oninput':'textAreaAdjust(this)', 'resize': False, 'autofocus': True, 'required': True, 'class': 'form-control', 'id': 'input-1', 'name': 'message','placeholder': '...'}))
+    message = forms.CharField(widget=forms.TextInput(attrs={'maxlength': 250, 'autofocus': True, 'required': True, 'class': 'form-control', 'id': 'input-1', 'name': 'message','placeholder': '...'}))
---
+++ spottedmaster/gymmaster/static/scripts.js
@@ -0,0 +1,14 @@
+  title = document.getElementsByClassName('label-text')[0]
+  tip = document.getElementsByClassName('tip')[0]
+  input = document.getElementById('input-1')
+  window.addEventListener("load", function() {
+    input.style.opacity = "1";
+    input.style.transform = "translate(-50%, -100%)";
+    input.style.pointerEvents = "auto";
+    input.style.transition = "all .4s cubic-bezier(.1, .45, .1, .85) .5s";
+    input.style.zIndex = "10";
+    tip.style.opacity = "1";
+    title.style.opacity = "1";
+    title.style.transform = "translate(-50%, -100%)";
+    title.style.transition = "all .3s cubic-bezier(.1, .45, .1, .85) .4s";
+  });
