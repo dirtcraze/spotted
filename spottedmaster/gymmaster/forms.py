3c15e7e947286e57128e115ca66fd3908b50593c
dirt_craze <kamil.porebski@icloud.com>
2023-01-22 00:10:31 +0100

    naprawiony frontend

--- spottedmaster/gymmaster/forms.py
+++ spottedmaster/gymmaster/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class MessageForm(forms.Form):
-    message = forms.CharField(widget=forms.TextInput(attrs={'maxlength': 250, 'autofocus': True, 'required': True, 'class': 'form-control', 'id': 'input-1', 'name': 'message', 'placeholder': '...'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'maxlength': 250, 'autofocus': True, 'required': True, 'id': 'message-area', 'class': 'form-control', 'name': 'message', 'placeholder': '...', 'rows': '1'}))
---
+++ spottedmaster/gymmaster/static/script.js
@@ -0,0 +1,58 @@
+// Czyszczenie pola tekstowego po wysłaniu wiadomości
+$('#message-area').val('');
+
+// Autodopasowywanie wysokości pola tekstowego
+autosize(document.querySelector('textarea'));
+
+// Nasłuchiwanie wysyłania formularza
+$(document).ready(function() {
+    $("#form").submit(function(e) {
+        // Zatrzymanie domyślnego zachowania formularza
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            data: $("#form").serialize(),
+            success: function() {
+                // Czyszczenie formularza
+                $("#form")[0].reset();
+                // Powrót do domyślnej wysokości pola tekstowego
+                $("#message-area")[0].style.height = '55px';
+                // Dodanie div z komunikatem o wysłaniu wiadomości
+                $("#alert-box").append(successAlert);
+                successAlert.hide().fadeIn(1000);
+            },
+            error: function() {
+                // Dodanie div z komunikatem o błędzie wysłania wiadomości
+                $("#alert-box").append(successAlert);
+                dangerAlert.hide().fadeIn(1000);
+            }
+        });
+    });
+});
+
+// Tworzenie div z komunikatem o wysłaniu wiadomości
+var successAlert = $("<div>", {
+    class: "alert success-alert",
+    html: "<h3>Wiadomość została wysłana</h3><a class=\"close\">&times;</a>"
+});
+
+// Tworzenie div z komunikatem o błędzie
+var errorAlert = $("<div>", {
+    class: "alert error-alert",
+    html: "<h3>Wystąpił problem z wysłaniem</h3><a class=\"close\">&times;</a>"
+});
+
+// Nasłuchiwanie kliknięcia na przycisk "x"
+$("body").on("click", ".close", function(){
+    // Usuwanie div z komunikatem po kliknięciu na przycisk "x"
+    $(this).closest("div.alert").fadeOut(1000, function(){
+        $(this).remove();
+    });
+});
+
+// Automatyczne usuwanie div z komunikatem po 3 sekundach
+setInterval(function(){
+    $("div.alert").fadeOut(1000, function(){
+        $(this).remove();
+    });
+}, 9000);
