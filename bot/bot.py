83b3fd5ac5fb0250a757ae2bd887a26bf5785272
= <kubap1402@gmail.com>
2023-01-22 23:15:47 +0100

    pillow data

--- bot/bot.py
+++ bot/bot.py
@@ -3,15 +3,16 @@ import config as cfg
 import sqlite3
 from pillow.pillow import create_image
 import utils
-
+import datetime
 
 class Bot:
-    def __init__(self, login, password, table_name):
+    def __init__(self, login, password, table_name, ig_name):
         self.cl = Client()
         self.cl.login(login, password)
         self.user_id = self.cl.user_id_from_username(login)
-        utils.set_interval(lambda: self.release(
-            table_name), cfg.RELEASE_INTERVAL)
+        # utils.set_interval(lambda: self.release(
+            # table_name), cfg.RELEASE_INTERVAL)
+        self.release(table_name, ig_name)
 
     def upload_photo(self, path, caption):
         self.cl.photo_upload(path, caption)
@@ -19,17 +20,21 @@ class Bot:
     def album_upload(self, photos, caption):
         self.cl.album_upload(photos, caption)
 
-    def release(self, table_name):
+    def release(self, table_name, ig_name):
         self.con = sqlite3.connect("../spottedmaster/db.sqlite3")
         self.cur = self.con.cursor()
         photos = []
         rows = self.cur.execute("SELECT * FROM " + table_name +
                                 " WHERE is_posted = 0 ORDER BY created_at ASC LIMIT 5").fetchall()
         if len(rows) == 5:
+            counter = 0
             for row in rows:
+                counter +=1
                 print(row)
+                date_object = datetime.datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S.%f")
+                formatted_date = date_object.strftime("%d %B %Y \nGODZ. %H:%M")
                 path = create_image(
-                    row[1], row[3][row[3].find(" "):row[3].rfind(":")], row[0])
+                    row[1], formatted_date, counter, ig_name)
                 photos.append(path)
                 self.cur.execute("UPDATE " + table_name +
                                  " SET is_posted = 1 WHERE id = " + str(row[0]))
@@ -39,4 +44,4 @@ class Bot:
 
 
 fitnessForLife = Bot(cfg.FITNESSFORLIFE_USERNAME,
-                     cfg.FITNESSFORLIFE_PASSWORD, "gymmaster_fitnessforlife")
+                     cfg.FITNESSFORLIFE_PASSWORD, "gymmaster_fitnessforlife", "spotted_fitnessforlife")
--- bot/pillow/pillow.py
+++ bot/pillow/pillow.py
@@ -1,39 +1,35 @@
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
-def create_image(message, time, id):
-    image = Image.open("template.jpg")
+def create_image(message, time, id, instagram):
+    image = Image.open("pillow/template.jpg")
 
     draw = ImageDraw.Draw(image)
 
     # Definicja czcionki
-    timeFont = ImageFont.truetype("Roboto-Light.ttf", 22)
-    mainFont = ImageFont.truetype("Proxima.otf", 38)
-    idFont = ImageFont.truetype("Latinka.ttf", 1000)
+    timeFont = ImageFont.truetype("pillow/Roboto-Light.ttf", 22)
+    mainFont = ImageFont.truetype("pillow/Proxima.otf", 38)
+    idFont = ImageFont.truetype("pillow/Latinka.ttf", 1000)
 
 
-    # Testowe wiadomości
-    text = "Siłownia to miejsce, gdzie można poprawić swoją sylwetkę i zwiększyć siłę. Dzięki regularnym treningom możemy osiągnąć wymarzone rezultaty. Witaj Olu i Kubo! Zróbcie sobie dzisiaj ciężki trening, pamiętajcie o rozgrzewce i skupcie się na technice ćwiczeń. Pamiętajcie, że cierpliwość i konsekwencja przyniosą Wam sukces! Do zobaczenia na siłowni!"
-    text2 = "Siłownia to miejsce, gdzie można poprawić swoją sylwetkę i zwiększyć siłę. "
-    time = "26 WRZEŚNIA 2022 \nGODZ. 20:00"
-    instagram = "INSTAGRAM\n@spotted_fitnessforlife"
-    id = "7"
+    instagram = "INSTAGRAM\n@{0}".format(instagram)
 
     # Wyciąganie potrzebnych danych odnośnie rozmiaru
-    wrapped_text = textwrap.fill(text2.upper(), width=45)
-    id_width, id_height = draw.textsize(id, font=idFont)
+    wrapped_text = textwrap.fill(message.upper(), width=45)
+    id_width, id_height = draw.textsize(str(id), font=idFont)
     text_width, text_height = draw.textsize(wrapped_text, font=mainFont)
 
     x_id = (image.width - id_width) / 2
     y_id = (image.height - id_height - 200) / 2
     x_text = (image.width - text_width) / 2
-    y_text = (image.height - text_height - 70) / 2
-
+    y_text = (900 - text_height) / 2
+    if(text_height>200):
+        y_text = (820 - text_height) / 2
 
     # Dodawanie tekstu do zdjęcia
     draw.text((816, 890), time, font=timeFont, fill=(200, 200, 200), align='center', spacing=7)
     draw.text((50, 890), instagram, font=timeFont, fill=(200, 200, 200), align='center', spacing=7)
-    draw.text((x_id, y_id), id, font=idFont, fill=(19, 19, 19), align='center', stroke_width=5, stroke_fill=(0, 0, 0))
+    draw.text((x_id, y_id), str(id), font=idFont, fill=(19, 19, 19), align='center', stroke_width=5, stroke_fill=(0, 0, 0))
     draw.text((x_text, y_text), wrapped_text, font=mainFont, fill=(220, 220, 220), align='center', spacing=15)
     path = "pillow/images/" + str(id) + ".jpg"
     image.save(path)
